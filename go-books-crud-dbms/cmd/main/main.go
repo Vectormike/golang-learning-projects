28fb3d3d2a6dcbaadccd51d31bb65ebcb3b363b6
Vectormike <victorjonah199@gmail.com>
2022-12-16 02:19:39 +0100

    ...

--- go-books-crud-dbms/cmd/main/main.go
+++ go-books-crud-dbms/cmd/main/main.go
@@ -1,5 +1,14 @@
+package main
+
 import (
 	"fmt"
 	"log"
 	"net/http"
 )
+
+func main() {
+	fmt.Print("Server is running on port 4000")
+	if err := http.ListenAndServe(":4000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
