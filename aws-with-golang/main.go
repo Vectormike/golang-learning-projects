3ab6ddef55a9a09d9ff2f698f21302b03b6c87e5
Vectormike <victorjonah199@gmail.com>
2023-01-24 19:54:07 +0100

    ...

--- aws-with-golang/main.go
+++ aws-with-golang/main.go
@@ -1,10 +1,23 @@
-package main 
+package main
 
 import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main {
-	
+type MyEvent struct {
+	name    string `json:"Your name"`
+	message string `json:"Birthday wish"`
+}
+
+type MyResponse struct {
+	Message string `json:"My response"`
+}
+
+func sendBirthdayMessage(event MyEvent) (MyMessage, error) {
+	return MyResponse{Message: fmt.Sprintf("Hi %s! %d.")}
+}
+
+func main() {
+	lambda.Start(sendBirthdayMessage)
 }
